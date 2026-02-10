4edc88f9af5604ba02ce11a833b3d79146d56006
andy <andy@cybercentric.co.uk>
2026-02-10 10:50:10 +0000

    feat: improve image processing

--- backend/src/routes/file.routes.ts
+++ backend/src/routes/file.routes.ts
@@ -8,4 +8,11 @@ export const FILE_ROUTES = Router();
 FILE_ROUTES.delete('/api/images/:name', FileApi.deleteImageFiles);
 FILE_ROUTES.post('/api/images/edit', FileApi.editImageFiles);
 FILE_ROUTES.post('/api/images/upload', uploadImage.single('file'), FileApi.uploadImageFiles);
+
+/*
+ * Use the following route to resize all existing images
+ * Commented out as only really need if there are a lot of unedited pictures
+FILE_ROUTES.get('/api/images/reset', FileApi.resetAllImageFiles);
+ */
+
 FILE_ROUTES.get('/api/images', FileApi.getImageFiles);
